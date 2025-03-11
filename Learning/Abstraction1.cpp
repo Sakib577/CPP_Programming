3d8ea079f2f79e3c65e8b47e6cd3bd01808b6cac
Abdullah As Sakib <sakib5800.sk.3@gmail.com>
2025-03-11 11:44:16 +0600

    Implement AbstractEmployee class and Employee subclass with promotion logic

---
+++ Learning/Abstraction1.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+class AbstractEmployee{
+    virtual void AskForPromotion()=0; 
+    // by doing this we can make it obligatory (which means
+    // any classes that uses AbstractEmployee, needs to implement this method)
+
+};
+
+class Employee: AbstractEmployee{
+    private:
+        string name;
+        int age;
+
+    public:
+    Employee(string name, int age){
+        this->name = name;
+        this->age = age;
+    }
+
+    string getName(){
+        return name;
+    }
+    int getAge(){
+        return age;
+    }
+
+    void AskForPromotion(){
+        if (age > 30)
+        {
+            cout << name << " is promoted" << endl;
+        } else {
+            cout << name << " is not promoted" << endl;
+        }
+        
+    }
+};
+int main(){
+    Employee employee1("Sohan", 22);
+    cout << employee1.getName() << "\n" << employee1.getAge() << endl;
+
+    employee1.AskForPromotion();
+
+    return 0;
+}
