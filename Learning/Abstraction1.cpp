fe6bdc7c31a00f2c04a60f6776816b70455b732d
Abdullah As Sakib <sakib5800.sk.3@gmail.com>
2025-03-21 11:59:42 +0600

    Add comments to AbstractEmployee class to clarify its role as a blueprint for subclasses

--- Learning/Abstraction1.cpp
+++ Learning/Abstraction1.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 using namespace std;
 
+    // a class that cannot be instantiated directly, 
+    // serving as a blueprint for other classes (subclasses) to inherit from
+
 class AbstractEmployee{
     virtual void AskForPromotion()=0; 
     // by doing this we can make it obligatory (which means
     // any classes that uses AbstractEmployee, needs to implement this method)
-    // a class that cannot be instantiated directly, 
-    // serving as a blueprint for other classes (subclasses) to inherit from
 
 };
 
