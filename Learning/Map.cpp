f0736a8df9d9b6eeb8dcdb63c11bd83e6259d7a6
Abdullah As Sakib <sakib5800.sk.3@gmail.com>
2025-04-17 02:19:54 +0600

    Add a basic example of using std::map in C++
    - Implemented a simple program demonstrating the usage of std::map to store and manipulate GPA data.
    - Included examples of adding, accessing, modifying, and removing elements from the map.
    - Demonstrated the use of count and empty methods to check for existence and emptiness of the map.

---
+++ Learning/Map.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main()
+{
+    // structure:
+    // map<keytype, valuetype> mapName
+    map<string, double> gpa = {{"Nibir", 3.86}, {"Sakib", 3.23}, {"Sohan", 3.20}};
+
+    cout << "GPA of nibir is: " << gpa["Nibir"] << endl;
+    // also accessible with .at()
+    cout << "GPA of Sakib is: " << gpa.at("Sakib") << endl;
+    gpa.at("Sohan") = 3.26; // gpa["Sohan"] = 3.26 does the same thing
+    cout << "GPA of Sohan is: " << gpa.at("Sohan") << endl;
+
+    // to add an element,
+    gpa["Sadik"] = 3.779;
+    // or,
+    gpa.insert({"Sadik", 3.78});
+    cout <<"GPA of Sadik is: " << gpa.at("Sadik") << endl;
+
+    // A map cannot have elements with equal keys. For example,
+    // if we try to add "Sadik" two times to the map, it will only keep the first one
+
+    // To remove specific elements from a map, you can use the .erase() function
+    gpa.erase("Sohan");
+    // cout << "GPA of Sohan is: " << gpa.at("Sohan") << endl;
+    // it will create an error now
+
+    // .clear()  clears out all the elements
+
+    // to check if an element exists
+    cout << "Does Sohan has a result?: " << gpa.count("Sohan") << endl;
+    // 0 because element doesn't exist anymore
+
+    cout << "Is map empty?: " << gpa.empty() << endl;
+    // because map is not empty.
+    return 0;
+}
