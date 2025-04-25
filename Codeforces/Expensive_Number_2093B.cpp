157eac05a0daa3018ff68e392587b6bca6a84949
Abdullah As Sakib <sakib5800.sk.3@gmail.com>
2025-04-25 19:43:34 +0600

    Add solution for Codeforces problem Ideal Generator (2093A)

---
+++ Codeforces/Expensive_Number_2093B.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <set>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    string snum;
+    while (t--)
+    {
+        cin >> snum;
+        set <int> order;
+
+        for (int i = 0; i < snum.length(); i++)
+        {
+            
+        }
+        
+        
+    }
+    
+    return 0;
+}
---
+++ Codeforces/Ideal_generator_2093A.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+
+    int k;
+    while (t--)
+    {
+        cin >> k;
+        if (k%2 == 0)
+        {
+            cout << "NO" << endl;
+        } else cout << "YES" << endl;
+        
+    }
+    
+}
