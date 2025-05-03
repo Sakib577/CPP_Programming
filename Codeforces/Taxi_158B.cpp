f3c4805775d6acf2f91baa8dbad1fee44fd2d840
Abdullah As Sakib <sakib5800.sk.3@gmail.com>
2025-05-03 22:23:50 +0600

    Refactor Taxi 158B solution to use vector for group counts and simplify taxi calculation logic

--- Codeforces/Taxi_158B.cpp
+++ Codeforces/Taxi_158B.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main()
@@ -6,7 +7,8 @@ int main()
     int n;
     cin >> n;
     int taxi = 0;
-    int queue = 0;
+
+    vector<int> sgrps(4); // for index 0, i'm leaving it unused
     while (n--)
     {
         int input;
@@ -18,23 +20,37 @@ int main()
         }
         else
         {
-            queue += input;
-            if (queue > 4)
-            {
-                queue = input;
-                taxi++;
-            }
-            else if (queue == 4)
-            {
-                taxi++;
-                queue = 0;
-                if (n == 1)
-                {
-                    taxi++;
-                }
-            }
+            sgrps[input]++;
         }
     }
+
+    int pair13 = min(sgrps[1], sgrps[3]);
+    sgrps[1] -= pair13;
+    sgrps[3] -= pair13;
+    taxi += pair13;
+
+    taxi += sgrps[2] / 2;
+    sgrps[2] %= 2;
+    if (sgrps[2] > 0)
+    {
+        taxi += sgrps[2];
+        if (sgrps[1] >= 2)
+        {
+            sgrps[1] -= 2;
+        }
+        else
+            sgrps[1] = 0;
+    }
+
+    if (sgrps[3] > 0)
+    {
+        taxi += sgrps[3];
+    }
+    if (sgrps[1] > 0)
+    {
+        taxi += ((sgrps[1]+3)/4);
+    }
+
     cout << taxi << endl;
     return 0;
 }
