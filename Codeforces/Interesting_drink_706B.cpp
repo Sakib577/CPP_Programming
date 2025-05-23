34eac29caee6959bd90f81d26e965f72c5280858
Abdullah As Sakib <sakib5800.sk.3@gmail.com>
2025-05-24 00:02:09 +0600

    Refactor code structure for improved readability and maintainability

--- Codeforces/Interesting_drink_706B.cpp
+++ Codeforces/Interesting_drink_706B.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 int main()
@@ -18,6 +19,16 @@ int main()
     for (int i = 0; i < days; i++)
     {
         cin >> monLimit[i];
+    }
+    
+    sort(prices.begin(),prices.end());
+
+    for (int i = 0; i < days; i++)
+    {
+        int count= upper_bound(prices.begin(),prices.end(), monLimit[i])-prices.begin();    // if monLimit = 10; then >> 
+                                                                                            // counting index 4 - index 0 = 4
+        
+        cout << count << endl;
     }
     
     
