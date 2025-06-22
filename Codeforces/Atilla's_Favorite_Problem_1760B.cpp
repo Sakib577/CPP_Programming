7eddf824104244a3f2cc7abdfad76301049199e3
Abdullah As Sakib <sakib5800.sk.3@gmail.com>
2025-06-22 23:02:07 +0600

    Add solution for Codeforces problem: Atilla's Favorite Problem (1760B)

---
+++ Codeforces/Atilla's_Favorite_Problem_1760B.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <set>
+#include <algorithm>
+#include <map>
+#include <climits>
+#include <vector>
+#include <unordered_map>
+#define fast_io                       \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(nullptr)
+#define all(v) v.begin(), v.end()
+#define revall(v) v.rbegin(), v.rend()
+#define deb(x) cout << #x << " = " << x << '\n';
+#define newl cout << endl
+typedef long long ll;
+typedef unsigned long long ull;
+
+using namespace std;
+
+int main()
+{
+    fast_io;
+    ll t;
+    cin >> t;
+    
+    while(t--){
+        ll n;
+        cin >> n;
+        string s;
+        cin >> s;
+        ll MX=0;
+        
+        for(ll i=0; i<n; i++){
+            if(s[i]> MX)MX=s[i];
+        }
+        cout << MX-'a'+1 << endl;
+    }
+    return 0;
+}
+
