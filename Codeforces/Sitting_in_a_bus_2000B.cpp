39bb887281d202da396dee3d400ab052c5a0c3fc
Abdullah As Sakib <sakib5800.sk.3@gmail.com>
2025-08-09 04:27:41 +0600

    Added implementations for various problems including Goblin deception, trip shopping, and primary task logic across multiple files

---
+++ Codeforces/Sitting_in_a_bus_2000B.cpp
@@ -0,0 +1,110 @@
+#include <bits/stdc++.h>
+#define fast_io                       \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(nullptr)
+#define all(v) v.begin(), v.end()
+#define revall(v) v.rbegin(), v.rend()
+#define deb(x) cout << #x << " = " << x << '\n';
+#define newl cout << endl
+typedef long long ll;
+typedef __int128_t ll128;
+typedef unsigned long long ull;
+
+using namespace std;
+
+void printBin(ll n){
+    for (ll i = 10; i >=0; i--) // for 11 bits i=10 to i=0
+    {
+        cout << ((n>>i)&1);
+    }
+    newl;
+}
+
+bool checkPow2(ll n){
+    if(n&(n-1)) return false;
+    else return true;
+}
+
+ll powof2(ll n){
+    return 1<<n;
+}
+
+bool isPrime(ll n){
+    if(n<2) return false;
+    else for (ll i = 2; i*i <= n; i++) if(n%i==0) return false;
+    return true;
+}
+
+ll binPow(ll a, ll b){
+    ll res=1;
+    while (b>0)
+    {
+        if(b&1) res*=a;
+        a*=a;
+        b>>=1;
+    }
+    return res;
+}
+
+ll modExp(ll a, ll b, ll mod){
+    a=a%mod;
+    ll res=1;
+    while (b>0)
+    {
+        if(b&1) res=(a*res)%mod;
+        a=(a*a)%mod;
+        b>>=1;
+    }
+    return res;
+}
+
+vector<vector<ll>> subsets(vector<ll>& nums) {
+    ll n=nums.size(); 
+    ll subsetCnt= (1<<n); // as number of subset is 2^n
+    vector<vector<ll>> subset;
+    for (ll i = 0; i < subsetCnt; i++)
+    {
+        vector<ll> v;
+        for (ll mask = 0; mask < n; mask++)
+        {
+            if(mask&(1<<i)) v.push_back(nums[i]);
+        }
+        subset.push_back(v);
+    }
+    return subset;
+}
+
+int main()
+{
+    fast_io;
+    ll t;
+    cin >> t;
+
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+        vector<ll> v(n);
+        map <ll,ll> freq;
+ 
+        for(ll &i: v) cin >> i;
+ 
+        freq[v[0]]++;
+        
+        bool yes=true;
+        for (ll i = 1; i < n; i++)
+        {
+            if(freq[v[i]+1] > 0 || freq[v[i]-1] > 0){
+                freq[v[i]]++;
+            } else {
+                yes=false;
+                break;
+            }
+        }
+        cout << (yes?"YES":"NO") << endl;
+        
+    }
+    
+
+   return 0;
+}
---
+++ Codeforces/Trip_Shopping_2127C.cpp
@@ -0,0 +1,124 @@
+#include <bits/stdc++.h>
+#define fast_io                       \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(nullptr)
+#define all(v) v.begin(), v.end()
+#define revall(v) v.rbegin(), v.rend()
+#define deb(x) cout << #x << " = " << x << '\n';
+#define newl cout << endl
+typedef long long ll;
+typedef __int128_t ll128;
+typedef unsigned long long ull;
+
+using namespace std;
+
+void printBin(ll n){
+    for (ll i = 10; i >=0; i--) // for 11 bits i=10 to i=0
+    {
+        cout << ((n>>i)&1);
+    }
+    newl;
+}
+
+bool checkPow2(ll n){
+    if(n&(n-1)) return false;
+    else return true;
+}
+
+ll powof2(ll n){
+    return 1<<n;
+}
+
+bool isPrime(ll n){
+    if(n<2) return false;
+    else for (ll i = 2; i*i <= n; i++) if(n%i==0) return false;
+    return true;
+}
+
+ll binPow(ll a, ll b){
+    ll res=1;
+    while (b>0)
+    {
+        if(b&1) res*=a;
+        a*=a;
+        b>>=1;
+    }
+    return res;
+}
+
+ll modExp(ll a, ll b, ll mod){
+    a=a%mod;
+    ll res=1;
+    while (b>0)
+    {
+        if(b&1) res=(a*res)%mod;
+        a=(a*a)%mod;
+        b>>=1;
+    }
+    return res;
+}
+
+vector<vector<ll>> subsets(vector<ll>& nums) {
+    ll n=nums.size(); 
+    ll subsetCnt= (1<<n); // as number of subset is 2^n
+    vector<vector<ll>> subset;
+    for (ll i = 0; i < subsetCnt; i++)
+    {
+        vector<ll> v;
+        for (ll mask = 0; mask < n; mask++)
+        {
+            if(mask&(1<<i)) v.push_back(nums[i]);
+        }
+        subset.push_back(v);
+    }
+    return subset;
+}
+
+int main()
+{
+    fast_io;
+    ll t;
+    cin >> t;
+
+    while (t--)
+    {
+        ll n,k;
+        cin >> n >> k;
+
+        vector<pair<ll,ll>> a(n);
+
+        for(auto &i: a) cin >> i.first;
+        for(auto &i: a) cin >> i.second;
+
+        for (ll i = 0; i < n; i++)
+        {
+            if(a[i].first > a[i].second) swap(a[i].first,a[i].second);
+        }
+        
+        
+        for (ll i = 0; i < k; i++)
+        {
+            sort(all(a));
+            ll minimum=min({a[0].first,a[1].first,a[0].second,a[1].second});
+            ll maximum=max({a[0].first,a[1].first,a[0].second,a[1].second});
+            if(minimum==a[0].first && maximum==a[0].second || maximum==a[0].first && minimum==a[0].second || minimum==a[1].first && maximum==a[1].second || maximum==a[1].first && minimum==a[1].second)
+                continue;
+            else{
+                swap(a[0].first,a[1].first);
+                if(a[0].first > a[0].second) swap(a[0].first,a[0].second);
+                if(a[1].first > a[1].second) swap(a[1].first,a[1].second);
+            }
+        }
+
+        ll sum=0;
+        for (ll i = 0; i < n; i++)
+        {
+            sum+=abs(a[i].first-a[i].second);
+        }
+
+        cout << sum << endl;
+    }
+    
+
+   return 0;
+}
